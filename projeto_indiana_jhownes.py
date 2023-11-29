827e88887557eb28256a7696c5f49d4ae03f0bf0
João Victor Veríssimo <joaoverissimosjdr@gmail.com>
2023-11-29 14:28:25 -0300

    first commit

---
+++ projeto_indiana_jhownes.py
@@ -0,0 +1,8 @@
+
+def calculadora (x,y):
+    x = lv_desejado = int(input('Qual level deseja alcançar? '))
+    y = lv_atual = int(input('Qual seu level atual? '))
+    res = x - y
+    print(res)
+
+calculadora (300, 275)
